771f6b583400c29b64efba5d893b3b2b1c1cf992
Vegard Skogstad <skogstadv@gmail.com>
2025-11-16 22:12:33 +0100

    Day 33

--- 26-50/matrix_add.cu
+++ 26-50/matrix_add.cu
@@ -1,22 +1,43 @@
 #include <cuda_runtime.h>
+#include <cuda_fp16.h>
 #include <stdio.h>
 
-__global__ void matrix_add(const float* A, const float* B, float* C, int N) {
-    // Thinking we could load and add directly to output
-    int tid = threadIdx.x;
-    int idx = blockDim.x * blockIdx.x + tid;
-    int stride = blockDim.x * gridDim.x;
-    int limit = N*N;
-    for (int i = idx; i < limit; i += stride) {
-        C[i] = A[i] + B[i];
-    }
+template<int T>
+__global__ void gemm(const half* A, const half* B, half* C, int M, int N, int K, float alpha, float beta) {
+    // naive to high performance implementation, Following along with https://siboehm.com/articles/22/CUDA-MMM
+    // This is my version of kernel 3. Prefer my indexing. Using smem and tiled read/write to memory. Gradually building up temp until we can write to output.
+    int BLOCKSIZE = T;
+    const int col = blockIdx.x * BLOCKSIZE + (threadIdx.x % BLOCKSIZE);
+    const int row = blockIdx.y * BLOCKSIZE + (threadIdx.x / BLOCKSIZE);
+    const int idx = row * N + col;
+    // Setting up tiles and loading to smem:
+    __shared__ half* A_s[T][T];
+    __shared__ half* B_s[T][T];
+
+    for (int t = t0; t < t_max; t+= T*T) {
+        //A_s[col][row] = A[row*]
+
+    //
+
+        if (row < M && col < N){
+            float acc = 0.0f;
+            for (int j = 0; j < K; j++) {
+                acc +=  __half2float((A[row*K + j] * B[N*j + col]));
+            }
+        }
+    //printf("%i: %f\n", idx, acc);
+    C[idx] = __float2half(alpha * acc + beta * __half2float(C[idx]));
+    
+}
+        
+
 }
 
-// A, B, C are device pointers (i.e. pointers to memory on the GPU)
-extern "C" void solve(const float* A, const float* B, float* C, int N) {
-    int threadsPerBlock = 256;
-    int blocksPerGrid = (N * N + threadsPerBlock - 1) / threadsPerBlock;
 
-    matrix_add<<<blocksPerGrid, threadsPerBlock>>>(A, B, C, N);
-    cudaDeviceSynchronize();
+// A, B, and C are device pointers
+extern "C" void solve(const half* A, const half* B, half* C, int M, int N, int K, float alpha, float beta) {
+    int T = 32;
+    dim3 threadsPerBlock(T*T);
+    dim3 numBlocks((T + N - 1) / T, (T + M - 1) / T);  // x, y
+    gemm<32><<<numBlocks, threadsPerBlock>>>(A, B, C, M, N, K, alpha, beta);
 }
--- README.md
+++ README.md
@@ -62,4 +62,6 @@ Day 31: Matrix add.
 
 Day 32: GEMM. Not finished.
 
-Day 33: Did a study session on siboehms amtmul block, worked a little bit more on GEMM kernel.
+Day 33: Did a study session on siboehms mamtmul block, worked a little bit more on GEMM kernel.
+
+Day 34: Completed two first parts of GEMM kernel. Working on shared memory.
